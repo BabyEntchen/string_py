96caf9df99f0951963e28f3639672a5dfa940531
BabyEntchen <baby_entchen@web.de>
2023-04-06 18:41:44 +0200

    Added pyproject.toml

--- examples/string_formatting.py
+++ examples/string_formatting.py
@@ -2,6 +2,24 @@ from string_py import Format
 
 print(Format.align(values={"Username:": "John", "Register Date:": "01.01.2001"}))
 # Output:
-
+#
 # Username:        John
 # Register Date:   01.01.2001
+
+print(Format.surround("Hello World", char="*"))
+# Output:
+#
+# ***************
+# * Hello World *
+# ***************
+
+print(Format.table([["Product", "Value", "Sold"], ["Carrot", "3.99$", "34"], ["Milk", "5$", "103"]]))
+# Output:
+#
+# ┌────────────────────────┐
+# │ Product │ Value │ Sold │
+# ├────────────────────────┤
+# │ Carrot  │ 3.99$ │ 34   │
+# │────────────────────────│
+# │ Milk    │ 5$    │ 103  │
+# └────────────────────────┘
