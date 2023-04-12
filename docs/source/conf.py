dc690c226feac169e2df264a2a822a350320e511
BabyEntchen <69644451+BabyEntchen@users.noreply.github.com>
2023-04-12 19:51:57 +0200

    v0.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'String-Py'
 copyright = '2023, BabyEntchen'
 author = 'BabyEntchen'
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
